c6452963754ad4679842916969663c1ef81f45a8
unknown <Administrator@FNYAKAGWA.mo-de.local>
2021-05-07 17:58:53 +0300

    add dynamic GET route

--- server.js
+++ server.js
@@ -6,15 +6,28 @@ const color = require('colour')
 //Mock JSON Data
 const mockUserData=[{name:'Mark'},{name:'Jill'}]
 
+//Test function
+const getId = () => {
+    return 2 * 4
+}
+
 //Get all users
-app.get('/users', (req,res) => {
+app.get('/users:id', (req,res) => {
     res.json({success: true,
         message: 'successfully got users. Nice!',
-        users: mockUserData
+        users: mockUserData,
          })
-    })
-
-
+})
+  
+//Get one user
+app.get('/users/:id', (req, res)=> {
+    console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`.inverse.yellow)
