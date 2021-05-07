40afea53e78e0000f53e5dfc6f6f91df236a6fba
unknown <Administrator@FNYAKAGWA.mo-de.local>
2021-05-07 17:36:29 +0300

    add first GET route

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
+const color = require('colour')
+
+//Mock JSON Data
+const mockUserData=[{name:'Mark'},{name:'Jill'}]
+
+//Get all users
+app.get('/users', (req,res) => {
+    res.json({success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+         })
+    })
 
 
 
 app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`)
+    console.log(`Server running on PORT ${PORT}`.inverse.yellow)
 })
